f43dae25624a75f9c39b542fda6f00e5e2e9a96d
Jorge <jorgemaster119@gmail.com>
2019-04-01 11:21:21 -0600

    Registro modificado y perfil

--- Controlador/Crearcuenta.php
+++ Controlador/Crearcuenta.php
@@ -1,29 +1,134 @@
 <?php
   include '../Controlador/Conexion.php';
 
-  $idUsuario = null;
-  $consulta="insert into usuario values('".$idUsuario."',
-  '".$_POST['user']."',
-  '".$_POST['ApePat']."',
-  '".$_POST['ApeMat']."',
-  '".$_POST['usuario']."',
-  '".$_POST['contrasena']."',
-  '".$_POST['direccion_Calle']."',
-  '".$_POST['direccion_Colonia']."',
-  '".$_POST['direccion_Numero']."',
-  '".$_POST['correo']."')";
-  $resultado =mysqli_query($mysqli, $consulta);
-
-  	if($resultado==1){
-      echo '<script> alert("Cliente registrado exitosamente")</script>';
-      echo '<script> window.location="../Vista/index.php"; </script>';
-
-  	}
-
-  	else{
-  		echo "Se ha producido un error revise los datos ingresados";
-      echo '<script> window.location="../Vista/VCliente.php"; </script>';
-  	}
 
 
+
+$direccion = $_POST['direccion_Calle'] . $_POST['direccion_Colonia'] . $_POST['direccion_Numero'] ;
+
+$nombreUsuario =   $_POST['user'];
+$nombreUsuario = trim(stripslashes(str_replace("'", "''", $nombreUsuario)));
+$apellidos =   $_POST['ApePat'] + $_POST['ApeMat'];
+$apellidos = trim(stripslashes(str_replace("'", "''", $apellidos)));
+$email= $_POST['correo'];
+
+$tipoUser = "cliente";
+
+$password = $_POST['contrasena'];
+
+$usuario = $_POST['usuario'];
+
+
+$estatus = 1;
+
+
+$sql = "{call SP_AltaCliente (?,?,? ,?, ? ,?, ?,?)}";
+//Pasamos los parametros a un arreglo 
+      $myparams['nombre'] = $nombreUsuario;
+      $myparams['apellidos'] = $apellidos;
+      $myparams['domicilio'] = $direccion;
+      $myparams['usuario'] = $usuario;
+      $myparams['correo'] =$email;
+      $myparams['contrasena'] = $password;
+      $myparams['tipoUsuario'] = "cliente";
+      $myparams['estatus'] = $estatus;
+    
+
+
+
+$params = array(   
+  array(&$myparams['nombre'], SQLSRV_PARAM_IN),  
+  array(&$myparams['apellidos'], SQLSRV_PARAM_IN),
+  array(&$myparams['domicilio'], SQLSRV_PARAM_IN),
+  array(&$myparams['usuario'], SQLSRV_PARAM_IN),
+  array(&$myparams['correo'], SQLSRV_PARAM_IN),
+  array(&$myparams['contrasena'], SQLSRV_PARAM_IN),
+  array(&$myparams['tipoUsuario'], SQLSRV_PARAM_IN),
+  array(&$myparams['estatus'], SQLSRV_PARAM_IN)
+);  
+
+
+  
+
+
+  //$stmt= sqlsrv_prepare( $conn, $sql, $params);  
+
+  if (!$conn) {
+    die( print_r( sqlsrv_errors(), true));
+ }
+ //sqlsrv_execute($stmt);
+
+ 
+    // $conn = get_sqlsrv_connect();
+     if ($conn === false) {
+         die(print_r(sqlsrv_errors(), true));
+     }
+     $stmt = sqlsrv_query($conn, $sql, $params);
+     if ($stmt === false) {
+         die(print_r(sqlsrv_errors(), true));
+     }
+     //
+     $rss = array();
+     do {
+         $rs = array();
+         while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+             array_push($rs, $row);
+         }
+         array_push($rss, $rs);
+     } while (sqlsrv_next_result($stmt));
+     sqlsrv_free_stmt($stmt);
+     //free_sqlsrv_conn($conn);
+     echo '<script> alert("Cliente registrado exitosamente")</script>';
+echo '<script> window.location="../Vista/index.php"; </script>';
+     return $rss;
+     echo '<script> alert("Cliente registrado exitosamente")</script>';
+     echo '<script> window.location="../Vista/index.php"; </script>';
+     
+
+
+
+  if( $stmt == false )  
+{  
+     echo "Error in executing statement 3.\n";  
+     die( print_r( sqlsrv_errors(), true)); 
+     echo "Se ha producido un error revise los datos ingresados";
+      echo '<script> window.location="../Vista/VCliente.php"; </script>'; 
+}  
+
+
+
+
+
+/*if(sqlsrv_execute($stmt)){
+
+  while($res = sqlsrv_next_result($stmt)){
+    // make sure all result sets are stepped through, since the output params may not be set until this happens
+  }
+  // Output params are now set,
+  print_r($myparams);
+  print_r($params);
+  
+  
+    
+
+
+}else{
+  die( print_r( sqlsrv_errors(), true));
+}
+*/
+//echo "$params\n";
+//sqlsrv_free_stmt( $stmt);  
+
+echo '<script> alert("Cliente registrado exitosamente")</script>';
+echo '<script> window.location="../Vista/index.php"; </script>';
+
+      
+sqlsrv_close( $conn); 
+
+echo '<script> alert("Cliente registrado exitosamente")</script>';
+echo '<script> window.location="../Vista/index.php"; </script>';
+   
+  	
+
+    
 ?>
--- Vista/perfil.php
+++ Vista/perfil.php
@@ -1,5 +1,92 @@
 <!DOCTYPE html>
 <html lang="mx" dir="ltr">
+<?php
+session_start();
+include '../Controlador/Validar.php';
+include '../Controlador/Conexion.php';
+
+
+$user_correo = $_SESSION['email'];
+//$user_pass = $_SESSION['pass'];
+
+
+$sqlSelect = "SELECT dbo.Persona.nombre, dbo.Persona.apellidos, dbo.Persona.domicilio, 
+dbo.Usuario.correo, dbo.Usuario.usuario, dbo.Usuario.contrasena
+FROM    dbo.Cliente INNER JOIN
+dbo.Persona ON dbo.Cliente.idPersona = dbo.Persona.idPersona INNER JOIN
+dbo.Usuario ON dbo.Cliente.idUsuario = dbo.Usuario.idUsuario AND 
+dbo.Persona.idPersona = dbo.Usuario.idPersona WHERE dbo.Usuario.correo = '$user_correo' 
+";
+
+$sqlDatos = "SELECT nombre, apellidos,
+correo , usuario,contrasena,domicilio 
+FROM vistaCliente WHERE correo = '$user_correo' and contrasena = '$user_pass' ";
+
+
+
+$res= sqlsrv_query($conn, $sqlSelect);
+$row = sqlsrv_num_rows($res);
+if( $res === false )
+{
+     echo "Error in statement preparation/execution.\n";
+     die( print_r( sqlsrv_errors(), true));
+}
+
+
+
+while($row = sqlsrv_fetch_Array($res,SQLSRV_FETCH_ASSOC)){
+    
+ 
+
+
+    $nameUs = $row['nombre'];
+  $userLastName= $row['apellidos'];
+  $userAddress = $row['domicilio'];
+  $user_correo= $row['correo'];
+  $userName= $row['usuario'];
+  $user_pass  = $row['contrasena'];
+
+
+    $_SESSION['user'] = $nameUs;
+    $_SESSION['user'] = $_POST['user'];
+    $_SESSION['ApePa'] = $userLastName;
+    $_SESSION['ApePa'] = $_POST['ApePa'];
+    $_SESSION['correo'] = $user_correo;
+    $_SESSION['correo'] = $_POST['correo'];
+    $_SESSION['usuario'] = $userName;
+    $_SESSION['usuario'] = $_POST['usuario'];
+    $_SESSION['contrasena'] = $user_pass;
+    $_SESSION['contrasena'] = $_POST['contrasena'];
+    $_SESSION['direccionCalle'] = $userAddress;
+    $_SESSION['direccionCalle'] = $_POST['direccionCalle'];
+  
+
+  
+
+}
+
+
+
+
+
+
+
+/*
+$nameUs = $row['nombre'];
+  $userLastName= $row['apellidos'];
+  $userAddress = $row['domicilio'];
+  $userEmail= $row['correo'];
+  $userName= $row['usuario'];
+  $userAddress = $row['contrasena'];
+  */      
+    
+
+
+sqlsrv_free_stmt( $res);
+
+
+?>
+
   <head>
     <meta charset="utf-8">
     <title>Perfil Cliente</title>
@@ -56,6 +143,7 @@
   <body>
 <!-- Menu-->
     <div class="menu-wrapper">
+    <form action = "../Controlador/MostrarDatos.php" >
     <div class="contenedor-menu">
       <ul id="menu">
         <li style=display:inline-block;float:right; padding:10px;> <a href="../Vista/historial.php">  <b>Historial</b> </a></li>
@@ -70,19 +158,18 @@
     <h2>Modificar Cuenta</h2>
       <div class="contenedor-input">
       <p>Nombre</p>
-      <input type="text"  name="user"  class="input-100"placeholder="Nombre" required>
+      <input type="text" id="user" name="user" value = "<?php echo $nameUs;?>" class="input-100"placeholder="Nombre" required>
       <p>Apellidos</p>
-      <input type="text" name="ApePat"  class="input-40"placeholder="Apellido Paterno" required>
-      <input type="text" name="ApeMat" class="input-40" placeholder="Apellido Materno" required>
+      <input type="text" id="ApePat" name="ApePat"  value="<?php echo $userLastName;?>" class="input-60"placeholder="Apellidos" required>
+      
       <p>Correo Electronico</p>
-   <input type="email" name="correo"  class="input-100" placeholder="Correo" required>
+   <input type="email" name="correo" value="<?php print $user_correo;?>" class="input-100" placeholder="Correo" required>
    <p>Usuario y Contraseña</p>
-   <input type="text"  name="usuario"  class="input-40" placeholder="Usuario" required required pattern="[A-Za-z0-9]{5,5}" title="Este campo requiere 5 caracteres: letras y numeros">
-   <input type="password"  name="contrasena"  class="input-40"placeholder="Contraseña"  required pattern="[A-Za-z0-9]{5,5}" title="Este campo requiere 5 caracteres: letras y numeros">
+   <input type="text"  name="usuario" value="<?php print $userName;?>" class="input-40" placeholder="Usuario" required required pattern="[A-Za-z0-9]{5,5}" title="Este campo requiere 5 caracteres: letras y numeros">
+   <input type="password"  name="contrasena" value="<?php print $user_pass;?>" class="input-40"placeholder="Contraseña"  required pattern="[A-Za-z0-9]{5,5}" title="Este campo requiere 5 caracteres: letras y numeros">
      <p>Direccion</p>
-       <input type="text" name="direccion_Calle"  class="input-30" placeholder="Calle" required>
-       <input type="text" name="direccion_Colonia"   class="input-30" placeholder="Colonia" required>
-       <input type="number" name="direccion_Numero"    class="input-30" placeholder="Numero" required>
+       <input type="text" name="direccionCalle"  value="<?php print $userAddress;?>" class="input-60" placeholder="Calle" required>
+      
 
     <input type="submit" name="btn_registrar" value="Modificar">
     </div>
